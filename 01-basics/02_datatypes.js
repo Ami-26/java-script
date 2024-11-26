eb6e8aed280a7e4aeaf2dbe6a49a974a7524ef58
Ami-26 <ernencirangadia26@gmail.com>
2024-11-26 10:14:00 +0000

    02_datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all js code as newar version..
+let name="ami"
+let no=1
+let sirname;
+let salary=null
+let isloggedin=false
+
+console.log(typeof no);
+console.log(typeof "ami")
+console.log(typeof sirname)
+console.log(typeof salary)
+console.log(typeof isloggedin)
+
+console.log(typeof undefined) // datatyp of undefine is UNDEFINED
+console.log(typeof null)  // datatype of null is OBJECT
+
+console.log(5*6)
+// alert(5*6) // throw an error because we use nodejs not use browser
+
+console.table([typeof "ami", typeof sirname, typeof salary, typeof no, typeof isloggedin, typeof null, typeof undefined])
