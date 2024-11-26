1f8e610adaa399fefa25b9510662d2e47eb04a1c
Ami-26 <ernencirangadia26@gmail.com>
2024-11-26 06:07:37 +0000

    01_variable

---
+++ 01-basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountid = 1
+let accountmail = "ami@26"
+var accountpwd = "123456"
+accountcity = "dhag"
+let accountstate
+console.log(accountid)
+
+console.table([accountid,accountmail,accountpwd,accountcity])
+// accountid = 2 inccorect way because constant variable value is not change
+
+console.table([accountid,accountmail,accountpwd,accountcity,accountstate])
