723688598a41077db8f52d0a7a24c7733ef8572d
Ami-26 <ernencirangadia26@gmail.com>
2024-12-04 12:27:39 +0000

    09_array.js

--- 01-basics/09_array.js
+++ 01-basics/09_array.js
@@ -12,18 +12,22 @@ console.log(myarray1.indexOf(11))
 console.log(myarray1.concat(myarray))
 console.log(myarray1.join("-"))
 
+// PUSH & POP operations are perfomed add & delete operations respectively at the end of an array
 console.log(myarray1.push(11))
 console.log(myarray1)
 console.log(myarray1.pop())
 console.log(myarray1)
 
+// UNSHIFT & SHIFT operations are performed add & delete operations respectively at the starting postion of an array
 console.log(myarray1.unshift(11))
 console.log(myarray1)
 console.log(myarray1.shift())
 console.log(myarray1)
 
+// SLICE operation give index value between start to end , after performed slice operation array same as before
 console.log(" SLICE ",myarray1.slice(2,6))
 console.log("after slice operation" , myarray1)
 
+// SPLICE operation give index value between start to end , after performed splice operation array should be diveded renaming array can show
 console.log(" SPLICE ",myarray1.splice(2,6))
 console.log("after splice operation" , myarray1)
