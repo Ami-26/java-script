dd1d4c0f942cea9180e59cce9fafd57808ab0c28
Ami-26 <ernencirangadia26@gmail.com>
2024-12-16 06:47:55 +0000

    01_if_else_demo.js

---
+++ 04-control_flow/01_if_else_demo.js
@@ -0,0 +1,48 @@
+let a=40
+// if (a<50) {
+//     console.log("yes");
+// }
+
+// if (a>50) {
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+    
+// }
+
+
+if (a>70) {
+    console.log("no1");
+    }
+    else if(a>60){
+        console.log("no2");
+    }
+    else if(a>50){
+        console.log("no3");
+    }
+    else if(no=40){
+        console.log("equal");
+    }
+ 
+const userloggedin = true
+const debitcard = true 
+
+if(userloggedin && debitcard){
+    console.log("you can elidgeble to purchase our course")
+}
+else{
+    console.log("oops! sorry something wrong...");
+    
+}
+
+const loggedingoogle = true
+const loggedinemail = false
+
+if(loggedingoogle || loggedinemail){
+    console.log(("hello welcome.. you are loggedin successfully.."));
+}
+else{
+    console.log("oops! something wrong..");
+    
+}
