6bf712876376cd542b5362b606f44b37dc66ec9b
Ami-26 <ernencirangadia26@gmail.com>
2024-12-16 07:29:33 +0000

    03_truthly_or_falsy_value.js

---
+++ 04-control_flow/03_truthly_or_falsy_value.js
@@ -0,0 +1,83 @@
+ // FALSY_VALUES :- 0,-0,"",null,undefined,NAN(not a number),Bigint 0n,false
+
+ // TRUTY_VALUES :- "0"," ","false",[],{},function(){}
+
+//  let email = "ami@gmail.com"
+
+//  if(email){
+//     console.log("you have an email");
+//     }
+//     else{
+//         console.log("you don't have an email");
+        
+//     }
+
+// let email = ""
+
+//  if(email){
+//     console.log("you have an email");
+//     }
+//     else{
+//         console.log("you don't have an email");
+        
+//     }
+
+
+// let email = " "
+
+//  if(email){
+//     console.log("you have an email");
+//     }
+//     else{
+//         console.log("you don't have an email");
+        
+//     }
+
+// let email = "0"
+
+//  if(email){
+//     console.log("you have an email");
+//     }
+//     else{
+//         console.log("you don't have an email");
+        
+//     }
+
+// let email = 0
+
+//  if(email){
+//     console.log("you have an email");
+//     }
+//     else{
+//         console.log("you don't have an email");
+        
+//     }
+
+// let email = []
+
+//  if(email){
+//     console.log("you have an email");
+//     }
+//     else{
+//         console.log("you don't have an email");
+        
+//     }
+
+
+// how to check array is empty or not
+
+let email = []
+
+ if(email.length===0){
+    console.log("email is an empty");
+    }
+    
+// how to check object is empty or not
+
+let emptyobj = {}
+
+if(Object.keys(emptyobj).length===0){
+    console.log("object is an empty");
+    
+}
+    
