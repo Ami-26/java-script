12db4010690bc4a5b66a42794035627952ddc3d4
Ami-26 <ernencirangadia26@gmail.com>
2024-12-16 07:47:32 +0000

    04_nullish_coalescing_operator.js

---
+++ 04-control_flow/04_nullish_coalescing_operator_??.js
@@ -0,0 +1,29 @@
+// ++++++++++ NULLISH_COALESCING_OPERATOR(??) ++++++++++++++
+
+let val1 
+
+// val1 = 10 ?? 20
+
+// val1 = null ?? 20
+
+// val1 = undefined ?? 20
+
+// val1 = null ?? 20 ?? undefined
+
+// val1 = null ?? undefined ?? 20
+
+val1 = null ?? 200 ?? 30
+console.log(val1);
+
+
+// ++++++++++ TERNARY_OPERATOR(?) +++++++++++
+
+// condition ? true : false
+
+let marks = 55
+
+marks <=35 ? console.log("fail") : console.log("pass");
+
+
+
+
