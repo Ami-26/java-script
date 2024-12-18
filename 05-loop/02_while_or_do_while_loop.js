7e3290ef4e9d2a756eb322742f146b08510c0435
Ami-26 <ernencirangadia26@gmail.com>
2024-12-18 11:22:52 +0000

    02_+while_or_do_while.loop.js

---
+++ 05-loop/02_while_or_do_while_loop.js
@@ -0,0 +1,13 @@
+let i=0
+console.log("forward print number using WHILE loop" );
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+
+let j=10;
+console.log("backward print number using DO_WHILE loop" );
+do {
+    console.log (j);
+    j--
+} while (j>0);
